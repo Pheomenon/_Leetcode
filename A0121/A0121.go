5117aa16155201bbc6709f308e0a714739707f51
Gao <1149650442@qq.com>
2020-11-18 10:43:02 +0800

    买股票的最佳时机

---
+++ A0121/A0121.go
@@ -0,0 +1,50 @@
+/**
+给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+如果你最多只允许完成一笔交易（即买入和卖出一支股票一次），
+设计一个算法来计算你所能获取的最大利润。
+注意：你不能在买入股票前卖出股票。
+
+示例 1:
+输入: [7,1,5,3,6,4]
+输出: 5
+解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+
+示例 2:
+输入: [7,6,4,3,1]
+输出: 0
+解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+通过次数315,761提交次数572,536
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	i := []int{7, 1, 5, 3, 6, 4}
+	fmt.Println(maxProfit(i))
+}
+
+func maxProfit(prices []int) int {
+	n := len(prices)
+	dp_i_0, dp_i_1 := 0, math.MinInt64
+	for i := 0; i < n; i++ {
+		dp_i_0 = max(dp_i_0, dp_i_1+prices[i])
+		dp_i_1 = max(dp_i_1, -prices[i])
+	}
+	return dp_i_0
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
