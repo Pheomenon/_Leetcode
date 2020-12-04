dc1256631188c1addfe2985db16aded20a473143
Gao <1149650442@qq.com>
2020-12-04 15:16:32 +0800

    翻转二叉树

---
+++ A0226/A0226.go
@@ -0,0 +1,59 @@
+package main
+
+/*
+翻转一棵二叉树。
+示例：
+输入：
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+输出：
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+备注:
+这个问题是受到 Max Howell 的 原问题 启发的 ：
+
+谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
+通过次数165,922提交次数213,730
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/invert-binary-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func main() {
+
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root != nil {
+		left := invertTree(root.Left)
+		right := invertTree(root.Right)
+
+		root.Right = left
+		root.Left = right
+	}
+	return root
+}
