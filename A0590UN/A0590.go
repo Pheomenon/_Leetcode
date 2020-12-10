7db6c0d9afc1418f8d4be88554d2061430976fea
Gao <1149650442@qq.com>
2020-12-10 15:12:14 +0800

    柠檬水找零

---
+++ A0590UN/A0590.go
@@ -0,0 +1,37 @@
+package main
+
+/*
+给定一个 N 叉树，返回其节点值的后序遍历。
+
+例如，给定一个 3叉树 :
+返回其后序遍历: [5,6,3,2,4,1].
+
+说明: 递归法很简单，你可以使用迭代法完成此题吗?
+
+通过次数41,338提交次数55,030
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func main() {
+
+}
+
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Children []*Node
+ * }
+ */
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+func postorder(root *Node) []int {
+
+}
