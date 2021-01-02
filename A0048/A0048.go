ff595340b196dcbc10e96356f317d3a43f33c738
Gao <1149650442@qq.com>
2021-01-02 10:46:12 +0800

    旋转图像

---
+++ A0048/A0048.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+/*
+给定一个 n×n 的二维矩阵表示一个图像。
+将图像顺时针旋转 90 度。
+
+说明：
+你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+
+示例 1:
+给定 matrix =
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+原地旋转输入矩阵，使其变为:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+
+示例 2:
+给定 matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+],
+
+原地旋转输入矩阵，使其变为:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
+通过次数134,773提交次数187,195
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/rotate-image
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func main() {
+	matrix := [][]int{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12},
+		{13, 14, 15, 16},
+	}
+	rotate(matrix)
+
+	fmt.Println(matrix)
+}
+
+func rotate(matrix [][]int) {
+	for i := 0; i < len(matrix); i++ {
+		for j := i; j < len(matrix); j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+
+	for i := 0; i < len(matrix[0]); i++ {
+		for k := len(matrix); k > len(matrix)/2; k-- {
+			matrix[i][len(matrix)-k], matrix[i][k-1] = matrix[i][k-1], matrix[i][len(matrix)-k]
+		}
+	}
+}
