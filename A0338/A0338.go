91475edec5ee872231700d1fc8fd3791719cf2a6
Gao <1149650442@qq.com>
2021-01-02 10:46:01 +0800

    种群统计

---
+++ A0338/A0338.go
@@ -0,0 +1,47 @@
+package main
+
+/*
+给定一个非负整数num。对于0 ≤ i ≤ num 范围中的每个数字i，计算其二进制数中的 1 的数目并将它们作为数组返回。
+
+示例 1:
+
+输入: 2
+输出: [0,1,1]
+示例 2:
+
+输入: 5
+输出: [0,1,1,2,1,2]
+进阶:
+
+给出时间复杂度为O(n*sizeof(integer))的解答非常容易。但你可以在线性时间O(n)内用一趟扫描做到吗？
+要求算法的空间复杂度为O(n)。
+你能进一步完善解法吗？要求在C++或任何其他语言中不使用任何内置函数（如 C++ 中的__builtin_popcount）来执行此操作。
+通过次数65,683提交次数86,061
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/counting-bits
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func main() {
+
+}
+
+func countBits(num int) []int {
+	ans := make([]int, num)
+	ans = append(ans, 0)
+	for i := 1; i <= num; i++ {
+		ans[i] = popCount(i)
+	}
+	return ans
+}
+
+func popCount(x int) int {
+	x = x - ((x >> 1) & 0x5555555555555555)
+	x = (x & 0x3333333333333333) + ((x >> 2) & 0x3333333333333333)
+	x = (x + (x >> 4)) & 0x0f0f0f0f0f0f0f0f
+	x = x + (x >> 8)
+	x = x + (x >> 16)
+	x = x + (x >> 32)
+	return x & 0x7f
+}
