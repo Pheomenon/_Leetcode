1d11230b16913741ab31de6542b0d48ea7ef2139
Gao <1149650442@qq.com>
2021-01-05 17:18:20 +0800

    回文子串

---
+++ A0647/A0647.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+/*
+给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+
+具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+
+示例 1：
+
+输入："abc"
+输出：3
+解释：三个回文子串: "a", "b", "c"
+示例 2：
+
+输入："aaa"
+输出：6
+解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
+
+提示：
+
+输入的字符串长度不会超过 1000 。
+通过次数74,910提交次数115,843
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/palindromic-substrings
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func main() {
+	s := "abc"
+	fmt.Println(countSubstrings(s))
+}
+
+func countSubstrings(s string) int {
+	ans := 0
+	for i := 0; i < len(s); i++ {
+		for j := i; j < len(s); j++ {
+			if check(s[i : j+1]) {
+				ans++
+			}
+		}
+	}
+	return ans
+}
+
+func check(s string) bool {
+	i, j := 0, len(s)-1
+	for i < j {
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j++
+	}
+	return true
+}
