12b91c0f5f7f380a0f346ed03e4f3d4853a7b1d5
Gao <1149650442@qq.com>
2021-01-08 16:59:08 +0800

    旋转数组

---
+++ A0189/A0189.go
@@ -0,0 +1,66 @@
+package main
+
+/*
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+
+输入: [1,2,3,4,5,6,7] 和 k = 3
+输出: [5,6,7,1,2,3,4]
+解释:
+向右旋转 1 步: [7,1,2,3,4,5,6]
+向右旋转 2 步: [6,7,1,2,3,4,5]
+向右旋转 3 步: [5,6,7,1,2,3,4]
+示例 2:
+
+输入: [-1,-100,3,99] 和 k = 2
+输出: [3,99,-1,-100]
+解释:
+向右旋转 1 步: [99,-1,-100,3]
+向右旋转 2 步: [3,99,-1,-100]
+说明:
+
+尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+要求使用空间复杂度为 O(1) 的原地算法。
+通过次数205,728提交次数459,234
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/rotate-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func main() {
+	nums := []int{
+		1, 2,
+	}
+	k := 2
+	rotate(nums, k)
+}
+
+func rotate(nums []int, k int) {
+	if k == 0 {
+		return
+	}
+	if k >= len(nums) {
+		if k == len(nums) {
+			return
+		}
+		k -= len(nums)
+	}
+	next := len(nums) - k
+	if next > len(nums)-1 {
+		next = next - len(nums) - 1
+	} else if next < 0 {
+		next = next + len(nums) - 1
+	}
+	reserve(nums[next:])
+	reserve(nums[:next])
+	reserve(nums)
+}
+
+func reserve(nums []int) []int {
+	for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+	return nums
+}
