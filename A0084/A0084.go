f995daa8689a57335d2948e71db3a7c07b94fe1c
Gao <1149650442@qq.com>
2021-01-09 16:02:59 +0800

    柱状图中的最大矩形

--- A0084/A0084.go
+++ A0084/A0084.go
@@ -5,16 +5,8 @@ package main
 
 求在该柱状图中，能够勾勒出来的矩形的最大面积。
 
-以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
-
-
-
-
-
-图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
-
-
-
+以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
+图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
 示例:
 
 输入: [2,1,5,6,2,3]
@@ -25,6 +17,39 @@ package main
 链接：https://leetcode-cn.com/problems/largest-rectangle-in-histogram
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
-func main() {
 
+func main() {
+	height := []int{4, 2}
+	largestRectangleArea(height)
+}
+
+//单调栈
+
+func largestRectangleArea(heights []int) int {
+	heights = append([]int{0}, heights...)
+	heights = append(heights, 0)
+	stack := make([]int, 0)
+	stack = append(stack, 0)
+	ptr := 0
+	ans := 0
+
+	for i := 1; i < len(heights); i++ {
+		for ptr != 0 && heights[i] < heights[stack[ptr]] {
+			h := heights[stack[ptr]]
+			ptr--
+			ans = max(ans, (i-stack[ptr]-1)*h)
+			stack = stack[:len(stack)-1]
+
+		}
+		ptr++
+		stack = append(stack, i)
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
