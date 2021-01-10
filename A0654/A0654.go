9dc6afe513ec3475ca22c7e01247bdb66a950a40
Gao <1149650442@qq.com>
2021-01-10 20:39:20 +0800

    最大查找树

--- A0654/A0654.go
+++ A0654/A0654.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 /*
 给定一个不含重复元素的整数数组 nums 。一个以此数组直接递归构建的 最大二叉树 定义如下：
 
@@ -8,11 +10,7 @@ package main
 右子树是通过数组中 最大值右边部分 递归构造出的最大二叉树。
 返回有给定数组 nums 构建的 最大二叉树 。
 
-
-
 示例 1：
-
-
 输入：nums = [3,2,1,6,0,5]
 输出：[6,3,5,null,2,0,null,null,1]
 解释：递归调用如下所示：
@@ -25,12 +23,10 @@ package main
     - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
         - 只有一个元素，所以子节点是一个值为 0 的节点。
         - 空数组，无子节点。
+
 示例 2：
-
-
 输入：nums = [3,2,1]
 输出：[3,null,2,null,1]
-
 
 提示：
 
@@ -43,6 +39,40 @@ nums 中的所有整数 互不相同
 链接：https://leetcode-cn.com/problems/maximum-binary-tree
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
+
 func main() {
 
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+	var build func(nums []int) *TreeNode
+	build = func(nums []int) *TreeNode {
+		if len(nums) == 0 {
+			return nil
+		}
+		max := math.MinInt64
+		tmp := 0
+		for i := 0; i < len(nums); i++ {
+			if max < nums[i] {
+				max = nums[i]
+				tmp = i
+			}
+		}
+		leftPart := nums[:tmp]
+		rightPart := nums[tmp+1:]
+		node := &TreeNode{
+			Val:   nums[tmp],
+			Left:  build(leftPart),
+			Right: build(rightPart),
+		}
+		return node
+	}
+	root := build(nums)
+	return root
 }
