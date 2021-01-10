77ed560d7fbe114e23a46bfba52e0d807c8e1239
Gao <1149650442@qq.com>
2021-01-10 22:30:30 +0800

    最后一块石头的重量

---
+++ A1302UN/A1302.go
@@ -0,0 +1,33 @@
+package main
+
+/*
+给你一棵二叉树，请你返回层数最深的叶子节点的和。
+
+示例：
+输入：root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+输出：15
+
+提示：
+
+树中节点数目在 1 到 10^4 之间。
+每个节点的值在 1 到 100 之间。
+通过次数13,109提交次数16,177
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/deepest-leaves-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+//func main() {
+//
+//}
+//
+//type TreeNode struct {
+//	Val   int
+//	Left  *TreeNode
+//	Right *TreeNode
+//}
+//
+//func deepestLeavesSum(root *TreeNode) int {
+//
+//}
