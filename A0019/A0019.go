fe4bdb7db0d79a0b76841349ae0f7bf5cb9435d9
Gao <1149650442@qq.com>
2021-01-20 10:40:37 +0800

    删除链表的倒数第n个节点

---
+++ A0019/A0019.go
@@ -0,0 +1,61 @@
+package main
+
+/*
+给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+进阶：你能尝试使用一趟扫描实现吗？
+
+示例 1：
+
+输入：head = [1,2,3,4,5], n = 2
+输出：[1,2,3,5]
+示例 2：
+
+输入：head = [1], n = 1
+输出：[]
+示例 3：
+
+输入：head = [1,2], n = 1
+输出：[1]
+
+提示：
+
+链表中结点的数目为 sz
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+通过次数309,691提交次数759,789
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+func main() {
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	curr := head
+	for n > 0 {
+		curr = curr.Next
+		n--
+	}
+	dummy := &ListNode{
+		0, head,
+	}
+	target := dummy
+
+	for curr != nil {
+		curr = curr.Next
+		target = target.Next
+	}
+
+	target.Next = target.Next.Next
+
+	return dummy.Next
+}
