31f8de1e981146cf8d348f3f7a2cc519af17579f
Gao <1149650442@qq.com>
2021-02-08 11:08:04 +0800

    电话号码的字母组合

--- A0017/A0017.go
+++ A0017/A0017.go
@@ -26,40 +26,35 @@ func main() {
 }
 
 func letterCombinations(digits string) []string {
-	var alphabet = []string{
-		"",
-		"",
-		"abc",
-		"def",
-		"ghi",
-		"jkl",
-		"mno",
-		"pqrs",
-		"tuv",
-		"wxyz",
+	if len(digits) == 0 {
+		return []string{}
 	}
-	var track string
-	ans := make([]string, 0)
+	ans := []string{}
+	alphbet := map[uint8]string{
+		2: "abc",
+		3: "def",
+		4: "ghi",
+		5: "jkl",
+		6: "mno",
+		7: "pqrs",
+		8: "tuv",
+		9: "wxyz",
+	}
 
-	var backtrack func(Kstart int, track string)
-	backtrack = func(Kstart int, track string) {
-
-		if len(track) == len(digits) {
+	var backtrack func(track, digits string)
+	backtrack = func(track, digits string) {
+		if len(digits) == 0 {
+			ans = append(ans, track)
 			return
 		}
 
-		for i := 0; i < len(digits); i++ {
-			for j := i; j < len(alphabet[digits[i]]); j++ {
-				track += alphabet[digits[j]]
-				for k := Kstart; k < len(alphabet[digits[k]]); k++ {
-					track += alphabet[digits[k]]
-					ans = append(ans, track)
-					track = track[:len(track)-len(alphabet[digits[k]])]
-				}
-				backtrack(Kstart+1, track)
-			}
+		for _, v := range alphbet[digits[0]-'0'] {
+			track += string(v)
+			backtrack(track, digits[1:])
+			track = track[:len(track)-1]
 		}
 	}
-	backtrack(0, track)
+	backtrack("", digits)
+
 	return ans
 }
