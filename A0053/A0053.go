9be64937024a2bc58e8e31760902acd865b6da76
Gao <1149650442@qq.com>
2021-02-23 22:07:50 +0800

    re:最大子序和

--- A0053/A0053.go
+++ A0053/A0053.go
@@ -24,16 +24,14 @@ func main() {
 }
 
 func maxSubArray(nums []int) int {
-	max := nums[0]
+	m := nums[0]
 	for i := 1; i < len(nums); i++ {
 		if nums[i]+nums[i-1] > nums[i] {
-			nums[i] += nums[i-1]
+			nums[i] = nums[i] + nums[i-1]
 		}
-		if nums[i] > max {
-			max = nums[i]
-		}
+		m = max(m, nums[i])
 	}
-	return max
+	return m
 }
 
 func max(x, y int) int {
